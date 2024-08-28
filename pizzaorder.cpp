e11203d01f288bdf3eaeed4a2f784528b2571319
zpoettker <poettkerzach@gmail.com>
2024-08-28 08:12:03 -0500

    Update pizzaorder.cpp

--- pizzaorder.cpp
+++ pizzaorder.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   
+   double numPizzas;
+   const double cost = 9.99000001;
+   
+   cout << "How many pizzas are being ordered: ";
+   cin >> numPizzas;
+   
+    double subTotal = numPizzas * cost;
+   
+   cout << endl << fixed << setprecision(2) << "Receipt:" << endl << "Subtotal: $" << subTotal << endl << "Total due: $" << subTotal * 1.06 << endl;
+
+   return 0;
+}
 
